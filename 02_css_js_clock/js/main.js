f235b8dbb532d259ca7f151aafab574b960be894
MehdiHan <meh.bouchard@outlook.fr>
2021-02-10 17:47:52 +0100

    adding text timer

--- 02_css_js_clock/js/main.js
+++ 02_css_js_clock/js/main.js
@@ -1,6 +1,30 @@
 const hourHand = document.querySelector('.hour__hand');
 const minuteHand = document.querySelector('.minute__hand');
 const secondHand = document.querySelector('.second__hand');
+const timer = document.querySelector('.timer');
+
+// timer div
+const timerContainer = document.createElement('p');
+const hourContainer = document.createElement('span');
+const minuteContainer = document.createElement('span');
+const secondContainer = document.createElement('span');
+
+timerContainer.append(hourContainer);
+timerContainer.append(minuteContainer);
+timerContainer.append(secondContainer);
+timer.append(timerContainer);
+
+const transformHand = (hours, minutes, seconds) => {
+  hourHand.style.transform = `rotate(${hours * 30}deg)`;
+  minuteHand.style.transform = `rotate(${minutes * 6}deg)`;
+  secondHand.style.transform = `rotate(${seconds * 6}deg)`;
+};
+
+const displayTimer = (hours, minutes, seconds) => {
+  hourContainer.textContent = `${hours} : `;
+  minuteContainer.textContent = `${minutes} : `;
+  secondContainer.textContent = seconds < 10 ? `0${seconds}` : seconds;
+};
 
 const howTimeIsIt = () => {
   const date = Date.now();
@@ -9,8 +33,9 @@ const howTimeIsIt = () => {
   const minutes = today.getMinutes();
   const seconds = today.getSeconds();
 
-  hourHand.style.transform = `rotate(${hours * 30}deg)`;
-  minuteHand.style.transform = `rotate(${minutes * 6}deg)`;
-  secondHand.style.transform = `rotate(${seconds * 6}deg)`;
+  transformHand(hours, minutes, seconds);
+  displayTimer(hours, minutes, seconds);
 };
 const timerId = setInterval(() => howTimeIsIt(), 1000);
+
+console.log(hours);
