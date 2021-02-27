629b99431d5c82d3815a0f29360bee0b51a23980
MehdiHan <meh.bouchard@outlook.fr>
2021-02-27 16:18:44 +0100

    adding pause function

--- 23_speech_synthesis/js/main.js
+++ 23_speech_synthesis/js/main.js
@@ -1,5 +1,7 @@
 const voicesDropdown = document.querySelector('[name="voice"]');
-const options = document.querySelectorAll('[type="range"],[name="text"]');
+const rate = document.querySelector('[name="rate"]');
+const pitch = document.querySelector('[name="pitch"]');
+const textarea = document.querySelector('[name="text"]');
 const speakButton = document.getElementById('speak');
 const stopButton = document.getElementById('stop');
 
@@ -18,24 +20,42 @@ const populateVoiceList = () => {
     createOptionElement(voice, index);
   });
 };
-const readSentence = () => {
+const speakSpeech = () => {
   const voices = speechSynthesis.getVoices();
-  const controls = Array.from(options);
-  const rateValue = controls[0].value;
-  const pitchValue = controls[1].value;
-  const textValue = controls[2].value;
   const selectedOption = voicesDropdown.selectedOptions[0];
   voices.forEach((voice) => {
     if (voice.name === selectedOption.dataset.name) {
       message.voice = voice;
     }
   });
-
-  message.rate = rateValue;
-  message.pitch = pitchValue;
-  message.text = textValue;
-
+  message.rate = rate.value;
+  message.pitch = pitch.value;
+  message.text = textarea.value;
+  speechSynthesis.speak(message);
+  if (event.type === 'change') {
+    toggle();
+  }
+};
+const toggle = () => {
+  speechSynthesis.cancel();
   speechSynthesis.speak(message);
 };
+const updateRate = (event) => {
+  message.rate = event.target.value;
+  toggle();
+};
+const updatePitch = (event) => {
+  message.pitch = event.target.value;
+  toggle();
+};
+const pauseSpeech = () => {
+  speechSynthesis.pause();
+};
+
 speechSynthesis.onvoiceschanged = populateVoiceList;
-speakButton.addEventListener('click', readSentence);
+
+speakButton.addEventListener('click', speakSpeech);
+stopButton.addEventListener('click', pauseSpeech);
+voicesDropdown.addEventListener('change', speakSpeech);
+rate.addEventListener('change', updateRate);
+pitch.addEventListener('change', updatePitch);
